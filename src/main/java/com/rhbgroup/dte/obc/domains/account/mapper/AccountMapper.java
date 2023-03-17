237b7edc248932daf901f55ba08330f38481e711
nhat <trinh.minh.nhat@nonsmtp.rhbgroup.com>
2023-03-17 11:58:53 +0700

    [CDRB-2865] update dml, add more mapping stuff

--- src/main/java/com/rhbgroup/dte/obc/domains/account/mapper/AccountMapper.java
+++ src/main/java/com/rhbgroup/dte/obc/domains/account/mapper/AccountMapper.java
@@ -2,22 +2,18 @@ package com.rhbgroup.dte.obc.domains.account.mapper;
 
 import com.rhbgroup.dte.obc.model.AccountModel;
 import com.rhbgroup.dte.obc.model.InitAccountRequest;
-import com.rhbgroup.dte.obc.model.UserModel;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.springframework.stereotype.Component;
 
 @Component
 @Mapper(componentModel = "spring")
 public interface AccountMapper {
 
-  default AccountModel toModel(InitAccountRequest request) {
-    return new AccountModel()
-        .mobileNo(request.getPhoneNumber())
-        .user(
-            new UserModel()
-                .loginType(request.getLoginType())
-                .username(request.getLogin())
-                .password(request.getKey()))
-        .bakongId(request.getBakongAccId());
-  }
+  @Mapping(source = "phoneNumber", target = "mobileNo")
+  @Mapping(source = "bakongAccId", target = "bakongId")
+  @Mapping(source = "login", target = "user.username")
+  @Mapping(source = "key", target = "user.password")
+  @Mapping(source = "loginType", target = "user.loginType")
+  AccountModel toModel(InitAccountRequest request);
 }
--- src/main/java/com/rhbgroup/dte/obc/domains/user/model/UserProfileModel.java
+++
@@ -1,4 +0,0 @@
-package com.rhbgroup.dte.obc.domains.user.model;
-
-// A resource server using REST (mainly communicate with CDRB)
-public class UserProfileModel {}
