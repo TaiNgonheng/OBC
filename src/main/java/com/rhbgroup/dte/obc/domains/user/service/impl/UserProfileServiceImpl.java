c45c38350b7090d949f0f3a6333241e776a0f661
nhat <trinh.minh.nhat@nonsmtp.rhbgroup.com>
2023-03-31 10:10:14 +0700

    [CDRB-2813] fix PR comments

--- src/main/java/com/rhbgroup/dte/obc/domains/user/service/impl/UserProfileServiceImpl.java
+++ src/main/java/com/rhbgroup/dte/obc/domains/user/service/impl/UserProfileServiceImpl.java
@@ -1,9 +1,10 @@
 package com.rhbgroup.dte.obc.domains.user.service.impl;
 
+import com.rhbgroup.dte.obc.common.ResponseMessage;
+import com.rhbgroup.dte.obc.domains.account.repository.AccountRepository;
 import com.rhbgroup.dte.obc.domains.user.repository.UserProfileRepository;
-import com.rhbgroup.dte.obc.domains.user.repository.entity.UserProfileEntity;
 import com.rhbgroup.dte.obc.domains.user.service.UserProfileService;
-import java.util.Optional;
+import com.rhbgroup.dte.obc.exceptions.BizException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,15 +13,30 @@ import org.springframework.stereotype.Service;
 public class UserProfileServiceImpl implements UserProfileService {
 
   private final UserProfileRepository userProfileRepository;
+  private final AccountRepository accountRepository;
 
   @Override
   public void updateBakongId(String username, String bakongId) {
 
-    Optional<UserProfileEntity> userProfileEntity = userProfileRepository.getByUsername(username);
-    if (userProfileEntity.isPresent()) {
-      UserProfileEntity userProfile = userProfileEntity.get();
-      userProfile.setBakongId(bakongId);
-      userProfileRepository.save(userProfile);
-    }
+    userProfileRepository
+        .getByUsername(username)
+        .ifPresent(
+            userProfile -> {
+
+              // if there is different bakongId
+              if (!bakongId.equals(userProfile.getBakongId())) {
+
+                // Verify account has been already linked
+                accountRepository
+                    .findByUserId(userProfile.getId())
+                    .ifPresent(
+                        account -> {
+                          throw new BizException(ResponseMessage.ACCOUNT_ALREADY_LINKED);
+                        });
+
+                userProfile.setBakongId(bakongId);
+                userProfileRepository.save(userProfile);
+              }
+            });
   }
 }
