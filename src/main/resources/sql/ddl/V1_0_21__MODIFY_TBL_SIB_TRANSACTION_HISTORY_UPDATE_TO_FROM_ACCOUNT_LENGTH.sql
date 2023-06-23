073896e6b6dbac61c3b48e5ab5b27fa22fa387cd
Laing Phearin <laingphearin@gmail.com>
2023-06-23 14:57:49 +0700

    alter table column name to be longer

---
+++ src/main/resources/sql/ddl/V1_0_21__MODIFY_TBL_SIB_TRANSACTION_HISTORY_UPDATE_TO_FROM_ACCOUNT_LENGTH.sql
@@ -0,0 +1,2 @@
+ALTER TABLE obc.tbl_obc_sibs_transaction_history MODIFY COLUMN `to_account` varchar(100) NOT NULL;
+ALTER TABLE obc.tbl_obc_sibs_transaction_history MODIFY COLUMN `from_account` varchar(100) NOT NULL;
