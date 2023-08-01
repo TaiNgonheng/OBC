5d3a974ce237cf9e58180d29ec961449145837f4
Laing Phearin <laingphearin@gmail.com>
2023-08-01 13:12:50 +0700

    change query field name to new fields

--- src/main/java/com/rhbgroup/dte/obc/domains/transaction/repository/query/TransactionHistoryQueries.java
+++ src/main/java/com/rhbgroup/dte/obc/domains/transaction/repository/query/TransactionHistoryQueries.java
@@ -5,8 +5,8 @@ public class TransactionHistoryQueries {
   private TransactionHistoryQueries() {}
 
   public static final String QUERY_TRANSACTION_HISTORY_BY_ACCOUNT_NUMBER =
-      "SELECT sth.id, sth.transfer_type, sth.transfer_message, sth.trx_id, sth.trx_amount, sth.trx_date, "
-          + "sth.trx_hash, sth.trx_status, sth.trx_ccy, sth.from_account, sth.to_account, sth.credit_debit_indicator, sth.channel_id "
+      "SELECT sth.id, sth.transfer_type, sth.transfer_message, sth.trx_id, sth.trx_amnt, sth.trx_date, "
+          + "sth.trx_hash, sth.trx_status, sth.tranCurr, sth.from_account, sth.to_account, sth.credit_debit_indicator, sth.channel_id "
           + "FROM tbl_obc_sibs_transaction_history sth "
           + "WHERE sth.from_account = :accNumber";
 
