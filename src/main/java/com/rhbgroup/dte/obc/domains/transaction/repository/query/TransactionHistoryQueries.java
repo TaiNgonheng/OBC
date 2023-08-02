48d00f472e843203dc64875e284b1f3d3e8ddcf0
Laing Phearin <laingphearin@gmail.com>
2023-08-02 11:11:37 +0700

    fix history sql query script

--- src/main/java/com/rhbgroup/dte/obc/domains/transaction/repository/query/TransactionHistoryQueries.java
+++ src/main/java/com/rhbgroup/dte/obc/domains/transaction/repository/query/TransactionHistoryQueries.java
@@ -5,9 +5,24 @@ public class TransactionHistoryQueries {
   private TransactionHistoryQueries() {}
 
   public static final String QUERY_TRANSACTION_HISTORY_BY_ACCOUNT_NUMBER =
-      "SELECT sth.id, sth.transfer_type, sth.transfer_message, sth.trx_id, sth.trx_amnt, sth.trx_date, "
-          + "sth.trx_hash, sth.trx_status, sth.tran_curr, sth.from_account, sth.to_account, sth.credit_debit_indicator, sth.channel_id "
-          + "FROM tbl_obc_sibs_transaction_history sth "
+      "SELECT sth.id,\n" +
+              "       sth.transfer_type,\n" +
+              "       sth.transfer_message,\n" +
+              "       sth.trx_id,\n" +
+              "       sth.trx_amount_in_acct_currency,\n" +
+              "       sth.trx_date,\n" +
+              "       sth.trx_hash,\n" +
+              "       sth.trx_status,\n" +
+              "       sth.currency_code,\n" +
+              "       sth.from_account,\n" +
+              "       sth.to_account,\n" +
+              "       sth.credit_debit_indicator,\n" +
+              "       sth.channel_id,\n" +
+              "       sth.trx_amnt,\n" +
+              "       sth.tran_curr,\n" +
+              "       sth.tran_fee_amnt,\n" +
+              "       sth.fee_amnt_in_acct_currency\n" +
+              "FROM tbl_obc_sibs_transaction_history sth "
           + "WHERE sth.from_account = :accNumber";
 
   public static final String COUNT_TRANSACTION_HISTORY_BY_ACCOUNT_NUMBER =
