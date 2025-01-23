5b222a55d0aa15cdea50cbf1aa135aa614ee1170
TaiNgonheng <heng68807@gmail.com>
2025-01-23 15:36:53 +0700

    Initial commit

--- src/main/java/com/rhbgroup/dte/obc/ObcApplication.java
+++ src/main/java/com/rhbgroup/dte/obc/ObcApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ObcApplication {
 
+
   public static void main(String[] args) {
     SpringApplication.run(ObcApplication.class, args);
   }
